9692eff574ebf31476e7353b331e32c0a6d544cb
Jean Lucas Ferreira de Sousa <jeanlfs@juntoseguros.com>
2023-05-21 20:26:33 -0300

    Added docker-compose file

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -44,7 +44,7 @@ func DbInstance() *mongo.Client {
 var Client *mongo.Client = DbInstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	var collection *mongo.Collection = client.Database("golangApi").Collection(collectionName)
 
 	return collection
 }
